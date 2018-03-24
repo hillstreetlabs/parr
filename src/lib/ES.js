87c9085de06126eee2713a99d5b05c9029402dae
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2018-03-24 01:53:58 -0700

    Add lib files

---
+++ src/lib/ES.js
@@ -0,0 +1,63 @@
+import Elasticsearch from "elasticsearch";
+
+export const indices = ["blocks", "transactions", "logs", "accounts"];
+
+export default class ES {
+  constructor() {
+    this.client = new Elasticsearch.Client({
+      host: process.env.ELASTICSEARCH_URL,
+      log: "error"
+    });
+  }
+
+  resetIndices() {
+    indices.map(async indexName => {
+      const indexExists = await this.client.indices.exists({
+        index: indexName
+      });
+      if (indexExists) await this.client.indices.delete({ index: indexName });
+      this.client.indices.create({ index: indexName });
+    });
+    return true;
+  }
+
+  bulkIndex(index, type, data) {
+    let bulkBody = [];
+
+    data.forEach(item => {
+      bulkBody.push({
+        index: {
+          _index: index,
+          _type: type,
+          _id: item.id
+        }
+      });
+
+      bulkBody.push(item);
+    });
+
+    return this.client
+      .bulk({ body: bulkBody })
+      .then(response => {
+        let errorCount = 0;
+        response.items.forEach(item => {
+          if (item.index && item.index.error) {
+            console.log(++errorCount, item.index.error);
+          }
+        });
+        console.log(
+          `Successfully indexed ${data.length - errorCount} out of ${
+            data.length
+          } items`
+        );
+      })
+      .catch(console.err);
+  }
+
+  indices() {
+    return this.client.cat
+      .indices({ v: true })
+      .then(console.log)
+      .catch(err => console.error(`Error connecting to the es client: ${err}`));
+  }
+}
---
+++ src/lib/Importer.js
@@ -0,0 +1,124 @@
+import Eth from "ethjs";
+import { action, computed, observable } from "mobx";
+
+export default class Importer {
+  @observable isRunning = false;
+  @observable imported = [];
+
+  constructor(db, options) {
+    this.db = db;
+    this.blockNumbers = options.block
+      ? [options.block]
+      : this.getBlockNumbers();
+  }
+
+  @computed
+  get importedStatus() {
+    if (this.blockNumbers.length == 0) return 0;
+    return this.imported.length / this.blockNumbers.length;
+  }
+
+  @action
+  async import() {
+    this.isRunning = true;
+    // Start
+    await Promise.all(
+      this.blockNumbers.map(blockNumber => {
+        return this.importBlock(blockNumber).then(block => {
+          this.imported.push(block);
+        });
+      })
+    );
+    await this.db.elasticsearch.bulkIndex("blocks", "block", this.imported);
+    // Stop
+    this.isRunning = false;
+  }
+
+  importBlock(blockNumber) {
+    return new Promise(async (resolve, reject) => {
+      const block = await this.db.web3.getBlockByNumber(blockNumber, true);
+      const parsedBlock = this.parseBlock(block);
+      parsedBlock.transactions = await Promise.all(
+        parsedBlock.transactions.map(async txn => {
+          const receipt = await this.db.web3.getTransactionReceipt(txn.hash);
+          txn.cumulativeGasUsed = receipt.cumulativeGasUsed.toString(10);
+          txn.gasUsed = receipt.gasUsed.toString(10);
+
+          let decoded;
+          try {
+            decoded = decoder(receipt.logs);
+          } catch (error) {
+            decoded = [];
+          }
+          txn.logs = receipt.logs.map((log, index) => {
+            return this.parseLog(log, decoded[index]);
+          });
+          return txn;
+        })
+      );
+      resolve(parsedBlock);
+    });
+  }
+
+  decodeIntegerField(hex) {
+    const result = hex.split("0x")[1];
+    return parseInt(result, 16);
+  }
+
+  decodeTimeField(field) {
+    return new Date(field.mul(new Eth.BN(1000)).toNumber(10)).toISOString();
+  }
+
+  parseLog(log, decoded = {}) {
+    return {
+      address: log.address,
+      data: log.data,
+      blockHash: log.blockHash,
+      blockNumber: log.blockNumber.toString(10),
+      logIndex: log.logIndex.toString(10),
+      removed: log.removed,
+      transactionHash: log.transactionHash,
+      transactionIndex: log.transactionIndex.toString(10),
+      decoded: decoded || {}
+    };
+  }
+
+  parseTransaction(transaction) {
+    return {
+      blockHash: transaction.blockHash,
+      blockNumber: transaction.blockNumber.toString(10),
+      from: transaction.from,
+      gas: transaction.gas.toString(10),
+      gasPrice: Eth.fromWei(transaction.gasPrice, "ether"),
+      hash: transaction.hash,
+      nonce: transaction.nonce.toString(10),
+      to: transaction.to,
+      transactionIndex: transaction.transactionIndex.toString(10),
+      value: Eth.fromWei(transaction.value, "ether"),
+      logs: []
+    };
+  }
+
+  parseBlock(block) {
+    return {
+      difficulty: block.difficulty.toString(10),
+      gasLimit: block.gasLimit.toString(10),
+      gasUsed: block.gasUsed.toString(10),
+      hash: block.hash,
+      miner: block.miner,
+      nonce: block.nonce,
+      number: block.number.toString(10),
+      parentHash: block.parentHash,
+      size: block.size.toString(10),
+      timestamp: this.decodeTimeField(block.timestamp),
+      transactions: block.transactions.map(transaction => {
+        return this.parseTransaction(transaction);
+      })
+    };
+  }
+
+  getBlockNumbers() {
+    // TODO
+    return [];
+  }
+}
