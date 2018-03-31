aa02b4a67203fbf840bd40275752ecc0cd9dcd08
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2018-03-30 22:33:13 -0700

    Simplify implementsAbi function

--- src/util/implementsAbi.js
+++ src/util/implementsAbi.js
@@ -1,9 +1,8 @@
 import EthjsAbi from "ethjs-abi";
 
 export default (abi, bytecode) => {
-  const bytecodeIncludes = abi.map(method => {
+  return abi.every(method => {
     const signature = EthjsAbi.encodeSignature(method).substring(2);
     return bytecode.indexOf(signature) >= 0;
   });
-  return bytecodeIncludes.every(bool => bool);
 };
