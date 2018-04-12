9ab0bc4206b9776c7c8b78d2c964561a81d16f7a
Feridun Mert Celebi <mertcelebi93@gmail.com>
2018-04-12 14:43:22 -0700

    Remove async/await from db.js

--- src/db.js
+++ src/db.js
@@ -6,14 +6,8 @@ import Knex from "knex";
 import knexConfig from "../knexfile";
 
 // Returns an object with references to various databases
-export default async () => {
-  const web3 = new Eth(
-    new HttpProvider(
-      `https://${process.env.INFURA_NETWORK}.infura.io/${
-        process.env.INFURA_KEY
-      }`
-    )
-  );
+export default () => {
+  const web3 = new Eth(new HttpProvider(process.env.JSON_RPC_URL));
 
   const elasticsearch = new ES();
 
