cf8ba85a8507a95afc416b4d3ab93410573ee308
Graham Kaemmer <graham.kaemmer@gmail.com>
2018-04-13 11:51:43 -0700

    Add deploy script and circleci config

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+yarn
+yarn build
+
+if [ -f server.pid ]
+then
+  SERVER_PID=`cat server.pid`
+  if ! kill $SERVER_PID > /dev/null 2>&1; then
+    echo "No process running at $SERVER_PID" >&2
+  else
+    echo "Killed process $SERVER_PID"
+  fi
+fi
+
+echo "Starting server..."
+yarn start > logs/production.log 2> logs/production.log &
+
+echo $! > server.pid
+echo "Started server with pid $!"
