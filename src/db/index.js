1d89c1953aa3682464408c4e947100edd6377f62
Graham Kaemmer <graham.kaemmer@gmail.com>
2018-04-16 13:33:24 -0700

    Add redis to db

--- src/db/index.js
+++ src/db/index.js
@@ -3,19 +3,30 @@ import Eth from "ethjs-query";
 import ES from "./ES";
 import Etherscan from "etherscan-api";
 import Knex from "knex";
+import Redis from "redis";
 import knexConfig from "../../knexfile";
+import { promisify } from "util";
+
+const promisifyRedis = () => {
+  const proto = Redis.RedisClient.prototype;
+  Object.keys(proto).forEach(method => {
+    if (typeof proto[method] === "function") {
+      proto[method + "Async"] = promisify(proto[method]);
+    }
+  });
+};
+
+promisifyRedis();
 
 // Returns an object with references to various databases
 export default () => {
   const web3 = new Eth(new HttpProvider(process.env.JSON_RPC_URL));
-
   const elasticsearch = new ES();
-
+  const redis = Redis.createClient();
   const etherscan = Etherscan.init(process.env.ETHERSCAN_KEY);
-
   const pg = Knex(knexConfig[process.env.NODE_ENV || "development"]);
 
   console.log("Started databases");
 
-  return { web3, elasticsearch, etherscan, pg };
+  return { web3, elasticsearch, etherscan, pg, redis };
 };
